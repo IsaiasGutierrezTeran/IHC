436d8598a3c324a26e1dc4e06d41a1c77456fc06
IsaiasGutierrezTeran <isaiasdavid2625@gmail.com>
2025-07-01 19:27:23 -0400

    primercommitvoz ia 01 07 25

---
+++ APP DE VOZ/ihc_smart_light_bulb-main/android/app/src/main/kotlin/com/example/ihc_smart_light_bulb/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.ihc_smart_light_bulb
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ APP DE VOZ/voz_ia/android/app/src/main/kotlin/com/example/voz_ia/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.voz_ia
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
